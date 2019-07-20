c2b6ae7d44ad69a8374fa7fef3304c70e5d2ea2f
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-07-20 11:54:30 -0700

    Update mcdr_test.sql

--- scripts/mcdr_test.sql
+++ scripts/mcdr_test.sql
@@ -8,6 +8,7 @@ SELECT
     deskjob DJ  
  WHERE 
  DJ.desk_id = DH.desk_id
- AND DH.employee_num = 33066 
+-- AND DH.employee_num = 33066 
+ AND DH.employee_num in (33066, 41678, 25653, 30041, 27548, 5121, 28603, 31561, 37883, 37388, 12564, 29981, 4526, 44675, 30754, 42878, 27945, 21747, 25226, 7187, 39719, 40039, 28006, 1244, 44161) 
  AND DH.desk_id_start_date <= DATE('2003-12-05') 
  AND DH.desk_id_end_date >= DATE('2003-12-05') ;
