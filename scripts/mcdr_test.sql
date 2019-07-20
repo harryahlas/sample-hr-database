d77d3aaf1f8663e40f624a9f407536ce7c8541d2
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-07-19 20:23:08 -0700

    Update mcdr_test.sql

--- scripts/mcdr_test.sql
+++ scripts/mcdr_test.sql
@@ -8,6 +8,6 @@ SELECT
     deskjob DJ  
  WHERE 
  DJ.desk_id = DH.desk_id
- AND DH.employee_num = 12417 
+ AND DH.employee_num = 15011 
  AND DH.desk_id_start_date <= DATE('2003-12-05') 
  AND DH.desk_id_end_date >= DATE('2003-12-05') ;
