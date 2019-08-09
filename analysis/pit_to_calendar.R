1bba9b9300ecb995f14cb50dec9c459628c51152
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-08 23:52:09 -0700

    Complete PIT to Calendar analysis

--- analysis/pit_to_calendar.R
+++ analysis/pit_to_calendar.R
@@ -14,15 +14,14 @@ library(lubridate)
 hrsample::hrsampleCreateSQLite("my_db.sqlite3")
 
 
-# Connect to database -----------------------------------------------------
-
+# Connect to database
 con <- dbConnect(SQLite(),'my_db.sqlite3')
-
 
 # Retrieve deskhistory (point in time table)
 dh <- dbGetQuery(con, "SELECT * FROM DESKHISTORY")
 
-# Convert to date
+## One of the drawbacks of SQLite is that it does not store date formats.  So we will store them as text here.
+# Convert SQLite date fields from text to date
 dh <- dh %>% 
   mutate(desk_id_start_date = as.Date(desk_id_start_date),
          desk_id_end_date = as.Date(desk_id_end_date))
@@ -44,10 +43,8 @@ month_list <- seq.Date(start_date, end_date, by = "month")
 
 # Iterate through each deskhistory row to populate each month actives
 
-i =1
 
-month_list[2]
-
+## Note we had to reassign the termination flag for months that they did not terminate
 # Find employees that were active for entire month or termed during that month
 # Example For 2010-01-31: if their end date is > 2010-01-01 and start date <= 2010-01-31 and not termed then keep
 for (i in 1:length(month_list)) {
@@ -55,7 +52,8 @@ for (i in 1:length(month_list)) {
   dh_trend_active <- dh %>% 
     filter(desk_id_start_date <= ceiling_date(month_list[i]),
            desk_id_end_date > month_list[i]) %>% 
-    mutate(trend_month = month_list[i])
+    mutate(trend_month = month_list[i],
+           termination_flag = 0)
   
   dh_trend_term <- dh %>% 
     filter(termination_flag == 1,
@@ -69,9 +67,10 @@ for (i in 1:length(month_list)) {
 }
 
 # Convert date columns for SQLite
-dh_trend_backup <- dh_trend # take this out
+#dh_trend_backup <- dh_trend # take this out
 dh_trend$desk_id_start_date <- format(dh_trend$desk_id_start_date, "%Y-%m-%d")
 dh_trend$desk_id_end_date <- format(dh_trend$desk_id_end_date, "%Y-%m-%d")
+dh_trend$trend_month <- format(dh_trend$trend_month, "%Y-%m-%d")
 
 # Next we'll add the job title to our data. Each desk_id has a job assigned to it.  That assignment is on the deskjob table
 # add Job title
@@ -81,118 +80,20 @@ dh_trend <- dh_trend %>%
 
 
 ## Having a hierarchy will make this table more valuable for reporting. Let's add org information. 
-## The Rollup view lists all the company's desk_ids 4 levels deep.
+## The Rollup view lists all the company's desk_ids 4 levels deep along with the level of each desk.
 ## We will use the lvl04_desk_id to join the company hierarchy data.
 ## NEED TO EXPLAIN BETTER
 # add rollup view
 ru <- dbGetQuery(con, "SELECT * FROM ROLLUP")
-dh_trendx <- dh_trend %>% 
-  left_join(ru)
+dh_trend <- dh_trend %>% 
+  left_join(ru, by = c("desk_id" = "lvl04_desk_id"))
 
+## Our last step is to upload the data to a new table as employee_trend
 #upload new table employee_trend
+dbWriteTable(con, "employee_trend", dh_trend, overwrite = TRUE)
 
+# See results
+dbGetQuery(con, "SELECT TREND_MONTH, COUNT(*) FROM EMPLOYEE_TREND WHERE TERMINATION_FLAG = 1 GROUP BY TREND_MONTH")
 
- 
-# Iterate through each deskhistory row to populate each month terms
- # if they termed and they termed in the same month as the month above then keep
-# Make sure terms are clearly marked
-
-# x - iterate through one row
-example_tm <- dh %>%
-  filter(employee_num == 21065) %>% 
-  filter(row_number() == 1)
-  
-seq.Date(as.Date(example_tm$desk_id_start_date), as.Date(example_tm$desk_id_end_date), by = "month")
-
-# Iterate through deskhistory and create monthly trend table
-
-
-# upload deskhistory_trend
-
-
-
-
-
-
-
-
-# Import data -------------------------------------------------------------
-temp_file <- tempfile(fileext = ".xlsx")
-req <- GET("https://raw.githubusercontent.com/harryahlas/sample-hr-database/master/data/JohnsonLitigationResearch.xlsx", 
-           # write result to disk
-           write_disk(path = temp_file))
-input_data <- read_excel(temp_file)
-
-# Remove column that will be replaced and change date format
-input_data <- input_data %>% 
-  select(-`Job Name`) %>% 
-  mutate(`Date of incident or notification` = 
-           as.Date(`Date of incident or notification`))
-
-
-# Connect to database -----------------------------------------------------
-con <- dbConnect(RSQLite::SQLite(), 
-                 'my_db.sqlite3')
-
-dbListTables(con)
-
-
-# Run test query ----------------------------------------------------------
-test_sql <- read_file("https://raw.githubusercontent.com/harryahlas/sample-hr-database/master/scripts/mcdr_test.sql")
-test_df <- dbGetQuery(con, test_sql)
-test_df
-
-# Retrieve single row using placeholders ----------------------------------
-# Import sql script with placeholders
-mvdr_sql_placeholder <- read_file("https://raw.githubusercontent.com/harryahlas/sample-hr-database/master/scripts/mcdr.sql")
-
-# Replace placeholders with sample employee_num and date
-mvdr_sql <- mvdr_sql_placeholder %>% 
-  gsub(pattern = '%EMP_ID%',
-       replacement = input_data$`Employee Number`[1],
-       x = .) %>% 
-  gsub(pattern = '%DATE_ID%',
-       replacement = input_data$`Date of incident or notification`[1],
-       x = .)
-
-# Retrieve data
-df_one_row <- dbGetQuery(con, mvdr_sql)
-
-df_one_row
-
-# Retrieve all data using placeholder -------------------------------------
-# Create empty tibble
-df <- tibble()
-
-for (i in 1:nrow(input_data)) {
-  # Replace placeholders with employee_num and date
-  mvdr_sql <- mvdr_sql_placeholder %>% 
-    gsub(pattern = '%EMP_ID%',
-         replacement = input_data$`Employee Number`[i],
-         x = .) %>% 
-    gsub(pattern = '%DATE_ID%',
-         replacement = input_data$`Date of incident or notification`[i],
-         x = .)
-  
-  # Retrieve data to temporary table
-  df_temp <- dbGetQuery(con, mvdr_sql)
-  
-  df_temp$`Date of incident or notification` <- as.Date(df_temp$`Date of incident or notification`)
-  df <- bind_rows(df, df_temp)
-}
-
-# Join retrieved data to input data ---------------------------------------
-output <- input_data %>% 
-  left_join(df %>% select(-desk_id),
-            by = c("Date of incident or notification", 
-                   "Employee Number" = "employee_num")) %>% 
-  replace_na(list(job_name = "not with company at this time")) %>% 
-  rename(`Job Name` = job_name)
-
-# Note: not including a disclaimer tab here though normally would
-
-# Export ------------------------------------------------------------------
-wb <- createWorkbook()
-addWorksheet(wb, "HR data needed with output")
-writeDataTable(wb, 1, output)
-saveWorkbook(wb, "Johnson litigation research with job_name.xlsx", TRUE)
+# Close connection
+dbDisconnect(con) 
