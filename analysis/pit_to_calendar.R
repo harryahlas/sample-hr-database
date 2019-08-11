7f565bf02ef71c5b4f9dc131a2da77eea4df6010
harryahlas <42125399+harryahlas@users.noreply.github.com>
2019-08-10 17:16:57 -0700

    Update pit_to_calendar.R

--- analysis/pit_to_calendar.R
+++ analysis/pit_to_calendar.R
@@ -1,6 +1,7 @@
 file.remove("my_db.sqlite3")
 # Basic ETL with R and SQL: Create Calendar Table from Point In Time Data 
 #Today we will dive into ETL: <li>Extract data from a point in time table, Transform it into a monthly table, and then Load it back to the database.
+# for pit vs calendar see here, and here
 
 # For this exercise we will use R and SQLite.  I like the simplicity of SQLite.  It is very fast for this type of database and easy to install, though there are some drawbacks, as we will touch upon.
 
@@ -24,6 +25,7 @@ library(lubridate)
 #devtools::install_github("harryahlas/hrsample")
 hrsample::hrsampleCreateSQLite("my_db.sqlite3")
 
+#<h1>Extract
 # Let's connect to the database.
 
 # Connect to database
@@ -34,12 +36,6 @@ con <- dbConnect(SQLite(),'my_db.sqlite3')
 dh <- dbGetQuery(con, "SELECT * FROM DESKHISTORY")
 
 
-# > dh %>% sample_n(3)
-# employee_num desk_id desk_id_start_date desk_id_end_date termination_flag
-# 3034        44762     507         2003-08-13       2004-08-02                0
-# 1503        33719     989         2000-07-24       2002-06-27                0
-# 8004        40217      45         2013-04-18       2016-05-21                1
-
 # Let's look a little closer at the <code>dh</code> (deskhistory) table.
 
 sample_employee_num <- sample(dh$employee_num,1)
@@ -49,33 +45,38 @@ dh %>% filter(employee_num == sample_employee_num)
 # 2         5970     274         2012-04-26       2014-05-23                0
 # 3         5970     990         2014-05-24       2016-03-12                1
 
-#you can see one row for each time employees was in a position or desk_id as it is called here. point in time
+# The deskhistory data is transactional. It shows one row for each instance an employee was in a desk_id (aka position). 
+# It includes a start date <code>desk_id_start_date</code> and end date <code>desk_id_end_date</code>. This is known as point in time data.
 
-## One of the drawbacks of SQLite is that it does not store date formats.  So we will store them as text here.
+#<h1>Transform
+
+# Our goal is to get this point in time data in to a monthly snapshot, which can be very useful for reporting and analysis.
+# The monthly table will have one row for each month the employee is in a desk_id.
+## One of the drawbacks of SQLite is that it does not store date formats.  So the first transform will be to convert the date fields from text into dates.
 # Convert SQLite date fields from text to date
 dh <- dh %>% 
   mutate(desk_id_start_date = as.Date(desk_id_start_date),
          desk_id_end_date = as.Date(desk_id_end_date)) %>% 
   select(-promotion_flag)
 
+# Next, let's create a placeholder data frame for our monthly calendar data
 # Create empty deskhistory_trend table (monthly calendar table)
 dh_trend <- tibble()
 
-  
 
-
-# Sample of one TM
-dh %>% filter(employee_num == 21065)
-
+#We need to create a list of all months that will be included in our calendar table data t
 # Get list of all months, by end of month
 start_date <- as.Date("1999-01-01")
 end_date <- as.Date("2018-12-30")  
-
 month_list <- seq.Date(start_date, end_date, by = "month")
 
-# Iterate through each deskhistory row to populate each month actives
+# Now we are ready to do the real work - creating the new data.  To do so, we will:
+# <li>Iterate through each month in our list of months
+# <li>Identify the employees that were active as of the last day of that month
+# <li>Identify the employees that terminated during that month
+# <li>Add rows to our table for these active and terminated employees
 
-
+# Here is the code.  We'll review it in more detail below.
 ## Note we had to reassign the termination flag for months that they did not terminate
 ## Also, we'll remove the promotion rows
 # Find employees that were active for entire month or termed during that month
@@ -98,6 +99,32 @@ for (i in 1:length(month_list)) {
   dh_trend <- bind_rows(dh_trend, dh_trend_active, dh_trend_term)
   
 }
+
+# A more detailed look at the code is below. Feel free to SKIP AHEAD (link).
+
+# <sub header>Inside the for loop
+# The first piece of the for loop specifies how many loops to run.  This is the number of months in our list of months (<code>month_list</code>) 
+# for (i in 1:length(month_list)) {
+  
+# Then we'll create a temporary data frame <code>dh_trend_active</code> that captures active employees for the month in the current iteration. 
+# The code looks at the <code>desk_id_start_date</code> and <code>desk_id_end_date</code> columns to determine if an employee was active during that month.
+I THINK desk_id_end_date > month_list[i]) NEEDS TO BE CEILING DATE
+dh_trend_active <- dh %>% 
+  filter(desk_id_start_date <= ceiling_date(month_list[i]),
+         desk_id_end_date > month_list[i]) %>%
+
+# The loop repeats a nearly identical process for terminated employees. The only difference is there 
+  dh_trend_term <- dh %>% 
+  filter(termination_flag == 1,
+         desk_id_end_date <= ceiling_date(month_list[i], "month"),
+         desk_id_end_date >= month_list[i]
+  ) %>% 
+  mutate(trend_month = month_list[i])
+
+# The next piece of this <em>dplyr chain</em> creates a new column for the month and recodes the <code>termination_flag</code> to 0 since these employees were all active during that month.
+  mutate(trend_month = month_list[i],
+         termination_flag = 0)
+
 
 # Convert date columns for SQLite
 #dh_trend_backup <- dh_trend # take this out
@@ -143,6 +170,8 @@ dh_trend <- dh_trend %>%
          job_name,
          depth,
          everything())
+
+#<h1>Load
 
 ## Our last step is to upload the data to a new table as employee_trend
 #upload new table employee_trend
